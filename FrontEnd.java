7251b87939e7dd74ae76793be1ade7c3ce38766c
rcope13 <71539008+rcope13@users.noreply.github.com>
2020-09-29 17:04:32 -0500

    Add files via upload
    quality of life changes

--- FrontEnd.java
+++ FrontEnd.java
@@ -1,3 +1,4 @@
+
 // --== CS400 File Header Information ==--
 // Name: Ryan Cope
 // Email: rlcope@wisc.edu
@@ -8,18 +9,28 @@
 // Notes to Grader: <optional extra notes>
 import java.util.Scanner;
 
+/**
+ * This class serves the purpose of acting as the interface that the user
+ * has access to for our program.
+ * 
+ * @author rlcope
+ *
+ */
 public class FrontEnd {
+	public static String command = null; // the string that the input command is stored as
+	public static Scanner scnr = new Scanner(System.in); // takes input from the user
+	public static String prompt = "Please input a command. To see the commands, type c.";
+	public static CourseReadingBackEnd2 backEndData = null; // the object that contains the hashtable and backend
+																// commands
 
-	public static void main(String[] args) {
-		boolean quit = false;
-		String command = null;
-		Scanner scnr = new Scanner(System.in);
-		String prompt = "Please input a command. To see the commands, type c.";
-		CourseReadingBackEnd2 backEndData = null;
-
-		// this is where you decide to create a new database or use the default
-		System.out.println("Now running Check Your Texts. Would you like create a new database or import the default database?"
-					+ " To create a new database, input n, and to use the default database, input d.");
+	/**
+	 * This method initializes an instance of backEndData with either a new, empty
+	 * hashtable, or a hashtable populated with values from a CSV.
+	 */
+	public static void initializeDatabase() {
+		System.out.println(
+				"Now running Check Your Texts. Would you like create a new database or import the default database?"
+						+ " To create a new database, input n, and to use the default database, input d.");
 		command = scnr.nextLine().toLowerCase().trim();
 		boolean pickedDatabase = false;
 		while (pickedDatabase == false) {
@@ -30,7 +41,10 @@ public class FrontEnd {
 				}
 			}
 			if (command.equals("n")) {
-				pickedDatabase = true;
+				backEndData = new CourseReadingBackEnd2();
+				if (backEndData != null) {
+					pickedDatabase = true;
+				}
 			}
 			if ((!command.equals("n")) && (!command.equals("d"))) {
 				System.out.println("Please input a valid command.");
@@ -38,12 +52,18 @@ public class FrontEnd {
 			}
 		}
 		System.out.println("Your database has been initialized.");
+	}
 
-		//actual running of program
+	/**
+	 * This method acts as the direct way that the user interacts with the program,
+	 * allowing them to invoke commands on backEndData object they created.
+	 */
+	public static void runProgram() {
+		boolean quit = false;
 		while (quit == false) {
 			System.out.println("\n" + prompt);
 			command = scnr.nextLine().toLowerCase().trim();
-			
+
 			// outputs information corresponding to the "c" command
 			if (command.equals("c")) {
 				System.out.println("List of Commands:");
@@ -57,7 +77,6 @@ public class FrontEnd {
 			if (command.equals("g")) {
 				System.out.println("You have chosen to get a course from an ISBN. Please input a valid ISBN.");
 				String inputISBN = scnr.nextLine().trim();
-//				System.out.println("run the getHashIndex(inputISBN) command");
 				System.out.println(backEndData.get(inputISBN));
 			}
 
@@ -92,7 +111,16 @@ public class FrontEnd {
 				System.out.println("Must use valid command.");
 			}
 		}
-
 	}
 
+	/**
+	 * This method runs the method which initializes the backEndData object and runs
+	 * the method that acts as the actual program.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		initializeDatabase();
+		runProgram();
+	}
 }
